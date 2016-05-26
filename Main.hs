96d30e9d00fff39569b13e6f96f2970a7a86f03b
Anders Lundström <anders@urkraft.se>
2016-05-26 07:56:57 +0200

    Initial Commit

---
+++ Main.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import System.IO
+import Data.Char(toUpper)
+-- import Database.MongoDB
+
+main :: IO ()
+main = do
+       inh  <- openFile "input.txt"  ReadMode
+       outh <- openFile "output.txt" WriteMode
+       mainloop inh outh
+       hClose inh
+       hClose outh
+
+mainloop :: Handle -> Handle -> IO ()
+mainloop inh outh =
+    do ineof <- hIsEOF inh
+       if ineof
+           then do
+                  putStrLn "Klar!"
+                  return ()
+           else do inpStr <- hGetLine inh
+                   hPutStrLn outh (map toUpper inpStr)
+                   mainloop inh outh
+
+-- parsa =
+  -- con <- connect $ host "127.0.0.1"
